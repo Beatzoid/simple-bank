b2e5e40a8f67aff0121a36bdb80b86ef7692b824
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2023-10-08 11:03:27 -0700

    ✨ Env Variables

--- src/main.go
+++ src/main.go
@@ -5,19 +5,20 @@ import (
 	"log"
 
 	"github.com/beatzoid/simple-bank/api"
+	"github.com/beatzoid/simple-bank/util"
 
 	db "github.com/beatzoid/simple-bank/db/sqlc"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../")
+
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
@@ -26,7 +27,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server:", err)
